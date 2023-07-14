86ec7fc6fe22758b82cdf9d784a98a3995e3f121
Anthony Maldonado <anthony@clay.cl>
2023-07-14 16:24:04 -0400

    less code and more control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { ObjToArrayPipe } from './ObjToArray.pipe';
+import { KeysToArrayPipe } from './KeysToArray.pipe';
 
 @NgModule({
   declarations: [	
     AppComponent,
-    DragDropComponent
+    DragDropComponent,
+    ObjToArrayPipe,
+    KeysToArrayPipe
    ],
   imports: [
     BrowserModule,
